5e1276ef0be95ab096d5f5cef34a2d38c300c8d2
YashVarpe05 <yashvarpe2005@gmail.com>
2025-07-11 23:12:48 +0530

    Add logo SVG and Tailwind CSS configuration
    - Added a new logo SVG file for "Campus Wealth" with specified dimensions and styling.
    - Created a Tailwind CSS configuration file with custom theme colors and border radius settings.

---
+++ app/(auth)/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthLayout = ({ children }) => {
+	return (
+		<div className="flex justify-center pt-40">
+			{/* <h1>AuthLayout</h1> */}
+			{children}
+		</div>
+	);
+};
+
+export default AuthLayout;
