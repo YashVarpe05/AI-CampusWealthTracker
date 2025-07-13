1981ee72fbf73af5ec545470b4468d4f58a7e4ae
YashVarpe05 <yashvarpe2005@gmail.com>
2025-07-14 03:39:27 +0530

    feat: add budget management and account chart components
    - Implemented budget management functionality with `getCurrentBudget` and `updateBudget` actions.
    - Created `AccountChart` component to visualize transaction history with bar charts.
    - Added `BudgetProgress` component to display and edit monthly budget with progress tracking.
    - Integrated Inngest for event handling with a basic setup in the API route and client.
    - Updated package.json to include new dependencies: `inngest` and `recharts`.

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,28 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import React from "react";
 import AccountCard from "./_components/account-card";
+import { BudgetProgress } from "./_components/budget-progress";
+import { getCurrentBudget } from "@/actions/budget";
 
 const DashboardPage = async () => {
 	const accounts = await getUserAccounts();
+
+	const defaultAccount = accounts?.find((account) => account.isDefault);
+
+	let budgetData = null;
+	if (defaultAccount) {
+		budgetData = await getCurrentBudget(defaultAccount.id);
+	}
+
 	return (
-		<div className="px-5">
+		<div className="space-y-8 ">
 			{/* budget progress */}
+			{defaultAccount && (
+				<BudgetProgress
+					initialBudget={budgetData?.budget}
+					currentExpenses={budgetData?.currentExpenses || 0}
+				/>
+			)}
 			{/* overview */}
 			{/* account grid */}
 			<div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
